c19dd7efd40bb30423a3fa9792c0625801f8ef77
Nick Bisschop <nick.bisschop@hva.nl>
2024-01-08 17:36:15 +0100

    work in progress physics

--- Assets/Scripts/MarioMovement.cs
+++ Assets/Scripts/MarioMovement.cs
@@ -4,20 +4,37 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    private Rigidbody2D rigidbodymario;
+    private Camera cameramanager;
+    private new Rigidbody2D rigidbody;
 
     private Vector2 velocity;
     private float inputAxis;
 
     public float moveSpeed = 8f;
+    public float maxJumpHeight = 5f;
+    public float maxJumpTime = 1f;
+    public float jumpForce => (2f * maxJumpHeight) / (maxJumpTime / 2f);
+    public float gravity => (-2f * maxJumpHeight) / Mathf.Pow((maxJumpTime / 2f), 2f);
+
+    public bool grounded { get; private set; }
+    public bool jumping { get; private set; }
     private void Awake()
     {
-        rigidbodymario = GetComponent<Rigidbody2D>();
+        rigidbody = GetComponent<Rigidbody2D>();
+        cameramanager = Camera.main;
     }
 
     private void Update()
     {
         HorizontalMovement();
+
+        grounded = rigidbody.Raycast(Vector2.down);
+
+        if (grounded) {
+            GroundedMovement();
+        }
+
+        ApplyGravity();
     }
 
     private void HorizontalMovement()
@@ -26,11 +43,36 @@ public class Movement : MonoBehaviour
         velocity.x = Mathf.MoveTowards(velocity.x, inputAxis * moveSpeed, moveSpeed * Time.deltaTime);
     }
 
+    private void GroundedMovement()
+    {
+        velocity.y = Mathf.Max(velocity.y, 0f);
+        jumping = velocity.y > 0f;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            velocity.y = jumpForce;
+            jumping = true;
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        bool falling = velocity.y < 0f || !Input.GetButton("Jump");
+        float multiplier = falling ? 2f : 1f;
+
+        velocity.y += gravity * multiplier * Time.deltaTime;
+        velocity.y = Mathf.Max(velocity.y, gravity / 2f);
+    }
+
     private void FixedUpdate()
     {
-        Vector2 position = rigidbodymario.position;
+        Vector2 position = rigidbody.position;
         position += velocity * Time.fixedDeltaTime;
 
-        rigidbodymario.MovePosition(position);
+        Vector2 leftEdge = cameramanager.ScreenToWorldPoint(Vector2.zero);
+        Vector2 rightEdge = cameramanager.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        position.x = Mathf.Clamp(position.x, leftEdge.x + 0.5f, rightEdge.x - 0.5f);
+
+        rigidbody.MovePosition(position);
     }
 }
